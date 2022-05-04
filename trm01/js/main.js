1998cbde1a71d4cf5c768fee317d387ab3e7b816
cwal8202 <104574997+cwal8202@users.noreply.github.com>
2022-05-04 15:00:52 +0900

    Add files via upload

--- trm01/js/main.js
+++ trm01/js/main.js
@@ -6,4 +6,11 @@ $(function () {
     vertical: true,
     dots: true,
   });
+
+  $(".mopen").on("click", function () {
+    $(".gnb").toggleClass("active");
+    $(".mopen").toggleClass("active");
+  });
 });
+
+// toggleClass 는 클래스를 땠다 붙였다 하는 것이다. toggleClass() 괄호 안의 클래스를 땠다 붙였다 하는것이며 위 코드 같은 경우 .mopen을 클릭했을때 gnb=메뉴의 active 클래스가 작동하고 .mopen의 active 클래스가 작동하게 된다. 한번 더 클릭하면 toggleClass의 성질로 다시 active클래스가 때져 원래의 gnb=메뉴 와 .mopen이 된다.
